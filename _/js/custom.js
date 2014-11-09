4482300c27e5fb9a13e0a92bae819f7848723e81
MrHobbie <mrhobbie@gmail.com>
2014-11-08 20:19:59 -0800

    Under Dev - Backup for safe keeping

---
+++ _/js/custom.js
@@ -0,0 +1,26 @@
+//jQuery to collapse the navbar on scroll
+$('#main-nav').hide();
+$('div#footer').hide();
+$(window).scroll(function() {
+	if ($(this).scrollTop() > 400)
+	{
+		$('#main-nav').fadeIn();
+		$('div#footer').fadeIn();
+	}
+	else
+	{
+		$('#main-nav').fadeOut();
+		$('div#footer').fadeOut();
+	}
+});
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+	$('a.page-scroll').bind('click', function(event) {
+		var $anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $($anchor.attr('href')).offset().top
+			}, 1500, 'easeInOutExpo');
+			event.preventDefault();
+    });
+	});
